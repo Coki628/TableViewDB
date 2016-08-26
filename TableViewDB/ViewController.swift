a7ddee43982fdf2931df0d5b039641d2709f8a07
Coki Kakegawa <srcn4frdm.c@ezweb.ne.jp>
2016-08-26 12:42:08 +0900

    Initial Commit

---
+++ TableViewDB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewDB
+//
+//  Created by Coki Kakegawa on 2016/08/26.
+//  Copyright © 2016年 CokiLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
